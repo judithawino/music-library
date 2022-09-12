6277fc7e27bffe0b9d0f9f7c90a3eb8566d20f63
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-09-12 22:09:41 +0300

    Modified to fetch song list

---
+++ src/components/SongList.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import Song from './Song';
+// import NewSong from './NewSong';
+
+export default function SongList(){
+    const [song, setSong] = useState([]);
+
+    function handleSongFetch(){
+        fetch ('http://localhost:9292/songs')
+        .then ((resp)=>resp.json())
+        .then ((data)=>{
+            setSong(data);
+        })
+    }
+    useEffect(handleSongFetch, [])
+
+    const songList = song.map((songs)=>(
+        <Song key={songs.id}
+        songObject = {songs}
+        />
+    ))
+    return (
+        <div>
+            <section>
+            <h1>Trending songs</h1>
+            {songList} 
+            </section>
+            
+                       
+        </div>
+    );
+    
+}
