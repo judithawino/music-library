1e56051ecb26add36fcdf14eaa4d94816a852e84
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-09-13 09:10:13 +0300

    Modified Styling

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import ArtistList from './ArtistList';
 // import AlbumList from './AlbumList';
 // import GenreList from './GenreList';
-// import SongList from './SongList';
+import SongList from './SongList';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className='navbar'>
-          <h1 className='navbar-brand'>Music Library</h1>
+              <div className='navbar' style={{background:'#27104e', color:'#FFFFFF'}}>
+          <h1 className='navbar-brand' style={{color:'#FFFFFF'}}>Music Library</h1>
           <Link className="nav-link active" aria-current="page" to="/">Artist</Link>
           <Link className="nav-link active" to="/album">Album</Link>
           <Link className="nav-link active" to="/songs">Songs</Link> 
@@ -18,8 +18,8 @@ function App() {
         </div>
         <Routes>
           <Route exact path="/" element={<ArtistList />}/>
-          {/* <Route exact path="/about" element={<About />}/>
-          <Route excat path="/services" element={<Services />}/> 
+          <Route exact path="/songs" element={<SongList />}/>
+          {/* <Route excat path="/services" element={<Services />}/> 
           <Route excat path="/feedbacks" element={<FeedbackCard />}/> 
           <Route excat path="/gallery" element={<GalleryList />}/> 
           <Route exact path="/contact" element={<Contact />} />       */}
