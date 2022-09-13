e9baf6277bc2444612e4f290e8742660eecb537c
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-09-13 09:11:32 +0300

    Modified with tables

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,26 +1,78 @@
 import React from 'react';
 
 export default function Song({songObject}){
-    const {title, has_lyrics, artist, genres} = songObject
+    const {title, artist, id} = songObject
+    
 
     return (
-    <div>
-      <h2>{title}</h2>
-      <p>Lyrics: {has_lyrics}</p>       
-      <h4>Artists</h4>
-      <ul>
-      {artist.map((artist) => (
-            <li key = {artist.id}>{artist.name}</li>
-        ))}                
-      </ul>
-      <h3>Genres</h3> 
-      <ul>
-      {genres.map((genre) => (
-            <li key = {genre.id}>{genres.genre_name}</li>
-        ))} 
-      </ul>  
-    </div>
-  );
-    
+    <table  className="table table-primary">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Artist</th>
+            <th scope="col">Song</th>
+            <th scope="col">Genre</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="table-secondary">
+            <th scope="row">{id}</th>
+            <td key = {artist.id}>{artist.name}</td>
+            <td>{title}</td>
+            <td>{artist.genres.map((genre) => (
+                    <li key = {genre.id} >{genre.genre_name}</li>
+                    ))} </td>
+          </tr>
+          </tbody>
+          </table>
+        
+        )
+    //     <table className="table">
+    //     <thead>
+    //       <tr>
+    //         <th scope="col">#</th>
+    //         <th scope="col">Artist</th>
+    //         <th scope="col">Song</th>
+    //         <th scope="col">Genre</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       <tr>
+    //         <th scope="row">1</th>
+    //         <td key = {artist.id}>{artist.name}</td>
+    //         <td>{title}</td>
+    //         <td>{artist.genres.map((genre) => (
+    //                 <li key = {genre.id} class="card-text">{genre.genre_name}</li>
+    //                 ))} </td>
+    //       </tr>
+    //       <tr>
+    //         <th scope="row">2</th>
+    //         <td>Jacob</td>
+    //         <td>Thornton</td>
+    //         <td>@fat</td>
+    //       </tr>
+    //       <tr>
+    //         <th scope="row">3</th>
+    //         <td colspan="2">Larry the Bird</td>
+    //         <td>@twitter</td>
+    //       </tr>
+    //     </tbody>
+    //   </table>
+    {/* <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">Song: {title}</h2>
+                <p key = {artist.id} class="card-text">Artist: {artist.name}</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">Artist's Genres</h3>
+                <ul>        
+                    {artist.genres.map((genre) => (
+                    <li key = {genre.id} class="card-text">{genre.genre_name}</li>
+                    ))} 
+                </ul> 
+            </div>
+        </div> */}
 
 }
