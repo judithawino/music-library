fc744485cbee1fa0c1910518581c2937f0a9cef0
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-09-13 09:11:06 +0300

    Modified with tables

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default function Artist({artistObject}){
-    const {name,birth_date, hometown, country, albums} = artistObject
+    const {id, name,birth_date, hometown, country, albums} = artistObject
 
     return (
     <div>
-      <h2>{name}</h2>
+      {/* <h2>{name}</h2>
       <p>Birth Date: {birth_date}</p>
       <p>Hometown: {hometown}</p>
       <p>Country: {country}</p> 
@@ -14,7 +14,33 @@ export default function Artist({artistObject}){
       {albums.map((album) => (
             <li key = {album.id}>{album.album_name}</li>
         ))}        
-      </ul>   
+      </ul>  */}
+      <table  className="table table-primary">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Birth Date</th>
+            <th scope="col">Hometown</th>
+            <th scope="col">Country</th>
+            <th scope="col">Albums</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="table-secondary">
+            <th scope="row">{id}</th>
+            <td >{name}</td>
+            <td>{birth_date}</td>
+            <td>{hometown}</td>
+            <td>{country}</td>
+            <td>{albums.map((album) => (
+                <li key = {album.id}>{album.album_name}</li>
+                ))}        
+            </td>
+          </tr>
+          </tbody>
+          </table>
+          
     </div>
   );
     
